89b252714bae1c81bb89a63dec9357a2de1662a7
Pradeep <cons-pradeep.misra@1mg.com>
2023-08-04 12:40:21 +0530

    Initial commit for sorting demo

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# list-sorting-react-app
+this repo takes care for list sorting in react application
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <hi>Sorting Demo</hi>
+    </div>
+  );
+}
+
+export default App;
